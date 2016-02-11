3d57a48a9bf141af9967c5b9b6b36446f5919c0e
Harvey <harvey.taylor@bluefruit.co.uk>
2016-02-11 13:09:05 +0000

    Can play As white or Black

--- Go Client.Tests/GoClient_Tests.cpp
+++ Go Client.Tests/GoClient_Tests.cpp
@@ -33,3 +33,44 @@ TEST(ConstructorTests, GoBoardBoard4by4)
 		{ empty, empty, empty, empty } };
 	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
 }
+
+TEST(PlacingStoneTests, playwhiteOn1_1)
+{
+	GoBoard Test(3);
+	board testBoard = {
+		{ empty, empty, empty },
+		{ empty, empty, empty },
+		{ empty, empty, empty } };
+	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
+	Test.PlayStone(1, 1, white);
+	testBoard[1][1] = white;
+	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
+}
+
+TEST(PlacingStoneTests, playblackOn1_1)
+{
+	GoBoard Test(3);
+	board testBoard = {
+		{ empty, empty, empty },
+		{ empty, empty, empty },
+		{ empty, empty, empty } };
+	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
+	Test.PlayStone(1, 1, black);
+	testBoard[1][1] = black;
+	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
+}
+
+TEST(PlacingStoneTests, playblackOn_0_0_andWhiteOn_1_1)
+{
+	GoBoard Test(3);
+	board testBoard = {
+		{ empty, empty, empty },
+		{ empty, empty, empty },
+		{ empty, empty, empty } };
+	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
+	Test.PlayStone(0, 0, black);
+	testBoard[0][0] = black;
+	Test.PlayStone(1, 1, white);
+	testBoard[1][1] = white;
+	EXPECT_EQ(testBoard, Test.ReturnGoBoardBoard());
+}
--- Go Client/Go Client.h
+++ Go Client/Go Client.h
@@ -7,6 +7,7 @@ class GoBoard
 public:
 	GoBoard(uint8_t goBoardSize);
 	uint8_t ReturnGoBoardSize();
+	void PlayStone(uint8_t X, uint8_t Y, stone);
 	board ReturnGoBoardBoard();
 
 private:
