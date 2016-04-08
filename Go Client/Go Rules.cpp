feaa408ba59d367308e560b56aa2b53f1208fb25
harvey taylor <harvey taylor>
2016-04-08 11:39:45 +0100

    working rules in complicated dead stones (capturing an eye now works)

--- Go Client/Go Rules.cpp
+++ Go Client/Go Rules.cpp
@@ -26,20 +26,20 @@ void Rules::DoRules(stone StoneChecking, GoBoard &goBoard)
 
 void Rules::KillGroup(uint8_t x, uint8_t y, GoBoard &goBoard)
 {
-	goBoard.PlayStone(x, y, empty);
+	goBoard.PlayStone(y, x, empty);
 	currentBoard = goBoard.ReturnGoBoard();
 	if (x < currentBoard.size() - 1)
 		if (currentBoard[y][x + 1] == currentStone)
-			KillGroup(x + 1, y, goBoard);
+			KillGroup(y, x + 1, goBoard);
 	if (y < currentBoard.size() - 1)
 		if (currentBoard[y + 1][x] == currentStone)
-			KillGroup(x, y + 1, goBoard);
+			KillGroup(y + 1, x, goBoard);
 	if (x)
 		if (currentBoard[y][x - 1] == currentStone)
-			KillGroup(x - 1, y, goBoard);
+			KillGroup(y, x - 1,  goBoard);
 	if (y)
 		if (currentBoard[y - 1][x] == currentStone)
-			KillGroup(x, y - 1, goBoard);
+			KillGroup(y - 1, x,  goBoard);
 }
 
 uint8_t Rules::CheckLibertiesOfAGroup(uint8_t x, uint8_t y)
@@ -47,6 +47,7 @@ uint8_t Rules::CheckLibertiesOfAGroup(uint8_t x, uint8_t y)
 	coordinate currentCoord;
 	currentCoord.x = x;
 	currentCoord.y = y;
+
 	for (uint8_t i = 0; i < checkedCoordinates.size(); i++)
 	{
 		if (currentCoord.x == checkedCoordinates[i].x && currentCoord.y == checkedCoordinates[i].y)
@@ -54,7 +55,7 @@ uint8_t Rules::CheckLibertiesOfAGroup(uint8_t x, uint8_t y)
 	}
 	checkedCoordinates.push_back(currentCoord);
 
-	CheckLiberties(x, y);
+	CheckLibertiesOfAStone(x, y);
 	if (x < currentBoard.size() - 1)
 		if (currentBoard[x + 1][y] == currentStone)
 			CheckLibertiesOfAGroup(x + 1, y);
@@ -70,7 +71,7 @@ uint8_t Rules::CheckLibertiesOfAGroup(uint8_t x, uint8_t y)
 	return liberties;
 }
 
-void Rules::CheckLiberties(uint8_t x, uint8_t y)
+void Rules::CheckLibertiesOfAStone(uint8_t x, uint8_t y)
 {
 	if (x < currentBoard.size()-1)
 		if (currentBoard[x + 1][y] == empty)
